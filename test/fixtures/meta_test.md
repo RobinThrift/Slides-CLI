dce122f94f71180ded260ebb784a5bb4238ceebb
Robin Thrift <robin@we-two-design.com>
2014-08-26 13:08:46 +0200

    added funciton to extract meta info from slide

---
+++ test/fixtures/meta_test.md
@@ -0,0 +1,4 @@
+--[
+name: Slides Name
+author: Slides Author
+]--
--- test/parse.js
+++ test/parse.js
@@ -7,6 +7,11 @@ require('mocha');
 describe('Basics', function() {
 
 
+    it('should extract meta information', function() {
+        var parsed = parse.extractMeta(fs.readFileSync('test/fixtures/meta_test.md', {encoding: 'utf8'}));
+        parsed.should.be.eql({ name: 'Slides Name', author: 'Slides Author' });
+    });
+
     it('split simple slides', function() {
         var out = parse.split(fs.readFileSync('test/fixtures/split_test.md', {encoding: 'utf8'}));
         out.should.be.eql([
